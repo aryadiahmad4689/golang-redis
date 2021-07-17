cf3f7a830e57f509e55a392d5553448cd5f1b06e
padaelocomunity2 <sahrulalfa45@gmail.com>
2021-07-18 07:51:44 +0800

    test2

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
 func main() {
 	// http.HandleFunc("/pokemonwithredis", getPokemonWithRedis)
 	// http.HandleFunc("/pokemonwithoutredis", getPokemonWithoutRedis)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	// log.Fatal(http.ListenAndServe(":3000", nil))
 }
